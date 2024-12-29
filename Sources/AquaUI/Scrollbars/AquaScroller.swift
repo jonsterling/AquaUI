84a1ea159a2bb36878061d0c41cc59814694d85e
Jon Sterling <jon@jonmsterling.com>
2024-12-29 14:22:59 +0000

    refactor AquaScroller initialisation

--- Sources/AquaUI/Scrollbars/AquaScroller.swift
+++ Sources/AquaUI/Scrollbars/AquaScroller.swift
@@ -11,36 +11,44 @@ import SwiftUI
 class AquaScroller : NSScroller {
   @ObservedObject var viewModel = AquaScrollerViewModel()
   
-  var slotHostingView: NSHostingView<AquaScrollerSlotView>?
-  var knobHostingView: NSHostingView<AquaScrollerKnobView>?
+  lazy var slotHostingView: NSHostingView<AquaScrollerSlotView> = {
+    NSHostingView(rootView: AquaScrollerSlotView())
+  }()
+  
+  lazy var knobHostingView: NSHostingView<AquaScrollerKnobView> = {
+    NSHostingView(rootView: AquaScrollerKnobView(viewModel: viewModel))
+  }()
+  
+  private func configureSubviews() {
+    addSubview(slotHostingView)
+    addSubview(knobHostingView)
+  }
   
   override init(frame frameRect: NSRect) {
     super.init(frame: frameRect)
-    slotHostingView = NSHostingView(rootView: AquaScrollerSlotView())
-    knobHostingView = NSHostingView(rootView: AquaScrollerKnobView(viewModel: viewModel))
-    self.addSubview(slotHostingView!)
-    self.addSubview(knobHostingView!)
+    configureSubviews()
   }
   
   required init?(coder: NSCoder) {
     super.init(coder: coder)
+    configureSubviews()
   }
   
   override func drawKnob() {
     let rect = rect(for: .knob)
     viewModel.scrollOffset = rect.origin.y
     viewModel.isActive = if let window = window { window.isKeyWindow } else { false }
-    knobHostingView?.frame = rect
+    knobHostingView.frame = rect
   }
   
   override func drawKnobSlot(in slotRect: NSRect, highlight flag: Bool) {
     var rect = slotRect
     rect.size.height += 1
-    slotHostingView?.frame = rect
+    slotHostingView.frame = rect
   }
   
   override func checkSpaceForParts() {
     super.checkSpaceForParts()
-    knobHostingView?.isHidden = usableParts != .allScrollerParts
+    knobHostingView.isHidden = usableParts != .allScrollerParts
   }
 }
