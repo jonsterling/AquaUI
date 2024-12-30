81df1e51f7f226b00c9b154b221d4f865a9951a0
Jon Sterling <jon@jonmsterling.com>
2024-12-30 18:18:38 +0000

    AquaWindow: add showAquaToolbarButton property

--- Sources/AquaUI/Windows/AquaWindow.swift
+++ Sources/AquaUI/Windows/AquaWindow.swift
@@ -9,26 +9,32 @@ import Cocoa
 
 /// A subclass of `NSWindow` with Aqua-styled traffic light control buttons.
 open class AquaWindow : NSWindow {
-  var toolbarObservation: NSKeyValueObservation?
-  let toolbarLozengeController = AquaToolbarLozengeViewController()
+  private var toolbarObservation: NSKeyValueObservation?
+  private let toolbarLozengeController = AquaToolbarLozengeViewController()
   
-  public func installToolbarLozengeAccessory() {
+  /// When `true`, the toolbar toggle button will appear in the right-hand corner of the window. This property supersedes the old `NSWindow.showsToolbarButton` property to restore the pre-10.7 toolbar button.
+  public var showsAquaToolbarButton: Bool = false {
+    didSet { updateToolbarLozengeVisibility() }
+  }
+  
+  private func updateToolbarLozengeVisibility() {
+    let shouldBeVisible = showsAquaToolbarButton && toolbar != nil
+    toolbarLozengeController.isHidden = !shouldBeVisible
+  }
+  
+  public override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
+    super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
     addTitlebarAccessoryViewController(toolbarLozengeController)
-    
+
     toolbarObservation = observe(\.toolbar?.isVisible, options: [.initial,.new]) { window, observedChange in
       Task { @MainActor in
-        if let toolbarVisible = observedChange.newValue {
-          self.toolbarLozengeController.isHidden = false
-          if let toolbarVisible {
-            self.toolbarLozengeController.toolbarShown = toolbarVisible
-          }
-        } else {
-          self.toolbarLozengeController.isHidden = true
+        self.updateToolbarLozengeVisibility()
+        if let toolbarVisible = observedChange.newValue, let toolbarVisible {
+          self.toolbarLozengeController.toolbarShown = toolbarVisible
         }
       }
     }
   }
-  
   
   private var trafficLightButtons: [AquaTrafficLightButton] {
     let types: [ButtonType] = [.closeButton, .miniaturizeButton, .zoomButton]
