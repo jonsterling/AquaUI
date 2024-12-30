7b93548be0a6057fc782bd8cf5c8ab36384c3d5a
Jon Sterling <jon@jonmsterling.com>
2024-12-30 11:13:05 +0000

    fix flashing of toolbar lozenge during state transition

--- Sources/AquaUI/Windows/Toolbar Lozenge/AquaToolbarLozengeView.swift
+++ Sources/AquaUI/Windows/Toolbar Lozenge/AquaToolbarLozengeView.swift
@@ -18,6 +18,8 @@ public struct AquaToolbarLozengeView: View {
   public var body: some View {
     let buttonStyle = AquaToolbarLozengeButtonStyle(isOn: viewModel.isOn)
     Button("") {
+      // Although the state of this view is ultimately controlled from outside in response to toolbar visibility changes, we also toggle the state here in order to avoid the awkward "flashing" caused by the transition sequence (pressed, off) -> (unpressed, off) -> (unpressed, on). By toggling the state locally, that sequence is replaced by (pressed, off) -> (unpressed, on).
+      viewModel.isOn.toggle()
       NSApp.sendAction(#selector(NSWindow.toggleToolbarShown(_:)), to: nil, from: self)
     }
     .buttonStyle(buttonStyle)
